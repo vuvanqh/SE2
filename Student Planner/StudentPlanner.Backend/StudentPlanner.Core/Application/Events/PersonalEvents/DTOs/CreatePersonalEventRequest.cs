77c705c52d916644eda6cb38618f521fef0621e7
BanhBaoz <q.vuvan05@gmail.com>
2026-03-15 14:09:13 +0100

    feat: add IPersonalEventService and update personal event DTOs

--- Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/DTOs/CreatePersonalEventRequest.cs
+++ Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/DTOs/CreatePersonalEventRequest.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace StudentPlanner.Core.Application.Events.PersonalEvents.DTOs
+namespace StudentPlanner.Core.Application.PersonalEvents;
+
+public record CreatePersonalEventRequest
 {
-    internal class CreatePersonalEventRequest
-    {
-    }
+    [Required] public string Title { get; set; } = null!;
+    [Required] public string? Description { get; set; }
+    [Required] public DateTime StartTime { get; set; }
+    [Required] public DateTime EndTime { get; set; }
+    [Required] public string? Location { get; set; }
 }
--- Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/DTOs/UpdatePersonalEventRequest.cs
+++ Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/DTOs/UpdatePersonalEventRequest.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace StudentPlanner.Core.Application.Events.PersonalEvents.DTOs
+namespace StudentPlanner.Core.Application.PersonalEvents;
+
+
+//keep them separate in case of any future changes
+public record UpdatePersonalEventRequest
 {
-    internal class UpdatePersonalEventRequest
-    {
-    }
+    [Required] public Guid Id { get; set; }
+    [Required] public string Title { get; set; } = null!;
+    [Required] public string? Description { get; set; }
+    [Required] public DateTime StartTime { get; set; }
+    [Required] public DateTime EndTime { get; set; }
+    [Required] public string? Location { get; set; }
 }
