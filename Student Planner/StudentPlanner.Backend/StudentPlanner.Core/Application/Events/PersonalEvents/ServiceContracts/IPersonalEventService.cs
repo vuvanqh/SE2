313ece9afd0c864a59ca7b0cc9624dc00e35a938
BanhBaoz <q.vuvan05@gmail.com>
2026-03-15 13:45:36 +0100

    refactor: reorganize application layer into feature-based structure

---
+++ Student Planner/StudentPlanner.Backend/StudentPlanner.Core/Application/Events/PersonalEvents/ServiceContracts/IPersonalEventService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentPlanner.Core.Application.PersonalEvents;
+internal interface IPersonalEventService
+{
+}
---
+++ Student Planner/StudentPlanner.Backend/StudentPlanner.Tests/Events/PersonalEventTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentPlanner.Tests
+{
+    internal class PersonalEventTests
+    {
+    }
+}
